0bd172797587c4ee84720a13b391a06452d721f4
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-11-09 19:23:51 -0300

    commit

--- gamemodes/flood/gamemode/custom/autorun.lua
+++ gamemodes/flood/gamemode/custom/autorun.lua
@@ -5,31 +5,8 @@ GM_Name = "flood"
 
 GM_Path = GM_Name .. "/gamemode/custom/"
 
-local prefix = "[GM-Custom]"
+local function incl(file) include(GM_Path .. file) end
 
-local m = {
-    SV = function(s)
-        MsgC( Color(56, 228, 255, 200), prefix, " ", Color(184, 246, 255, 200), tostring(s).."\n")
-    end,
-
-    CL = function(s)
-        MsgC( Color(255, 235, 56, 200), prefix, " ", Color(184, 246, 255, 200), tostring(s).."\n")
-    end,
-
-    SH = function(s)
-        MsgC( Color(167, 255, 167, 200), prefix, " ", Color(184, 246, 255, 200), tostring(s).."\n")
-    end
-}
-
-local function AddDir(dir)
-    dir = dir .. "/"
-
-    local files, directories = file.Find(GM_Name .. dir .. "*", "LUA")
-    for _, v in ipairs(files) do
-        if string.EndsWith(v, ".lua") then
-            include(v)
-            m.SV("RUNNING: " .. v)
-        end
-    end
-end
-AddDir("autorun")
+incl("pointshop2_build.lua")
+incl("pointshop2_init.lua")
+incl("ps2_kinv_init.lua")
