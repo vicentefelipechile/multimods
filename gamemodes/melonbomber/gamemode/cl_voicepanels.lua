7d34b1d56ba7922294a4bd1a358bfdf2b64ae89c
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-11-11 00:58:06 -0300

    Create cl_voicepanels.lua

---
+++ gamemodes/melonbomber/gamemode/cl_voicepanels.lua
@@ -0,0 +1,11 @@
+function GM:PlayerStartVoice(ply)
+
+	if ( !IsValid( g_VoicePanelList ) ) then return end
+
+	GAMEMODE:PlayerEndVoice( ply )
+    
+end
+
+function GM:PlayerEndVoice(ply)
+    
+end
