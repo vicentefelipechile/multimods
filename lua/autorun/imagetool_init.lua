4d1b628f8249dc4097d6e1be6c79eb04eb2e3f32
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-11-12 13:06:13 -0300

    Create imagetool_init.lua

---
+++ lua/autorun/imagetool_init.lua
@@ -0,0 +1,19 @@
+if ( engine.ActiveGamemode() == "melonbomber" ) then return end
+
+--[[
+        © Asterion Project 2021.
+        This script was created from the developers of the AsterionTeam.
+        You can get more information from one of the links below:
+            Site - https://asterionproject.ru
+            Discord - https://discord.gg/CtfS8r5W3M
+        
+        developer(s):
+            Selenter - https://steamcommunity.com/id/selenter
+
+        ——— Chop your own wood and it will warm you twice.
+]]--
+
+
+-- Инклаидаем основной файл
+include("imagetool/sh_batch.lua")
+AddCSLuaFile("imagetool/sh_batch.lua")
